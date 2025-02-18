a447e045d9d5acbb6398848a5a0778b2a273ba4b
iamnotloser <1689986307@qq.com>
2025-02-18 16:04:43 +0800

    add picocli 示例代码

---
+++ lyou-generator-basic/src/main/java/com/leeyou/cli/example/Login.java
@@ -0,0 +1,27 @@
+package com.leeyou.cli.example;
+
+import picocli.CommandLine;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+class Login implements Callable<Integer> {
+    @CommandLine.Option(names = {"-u", "--user"}, description = "User name")
+    String user;
+
+    @CommandLine.Option(names = {"-p", "--password"}, description = "Passphrase", interactive = true)
+    String password;
+
+    public Integer call() throws Exception {
+        System.out.println("password = " + password);
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Login()).execute("-u","user123","-p");
+    }
+
+
+}
